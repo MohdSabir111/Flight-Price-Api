210ca402147127416864f8e46a3076892c15255a
MOHD SABIR <sabiransari01981@gmail.com>
2023-06-06 00:52:50 +0530

    .

--- models/conn.js
+++ models/conn.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/Flight-Price')
+mongoose.connect('mongodb://localhost:27017/Flight-Price',{useNewUrlParser :true, useUnifiedTopology : true})
 .then(()=>{console.log('database connection is sucessfull');})
 .catch((error)=>{console.log(`${error} not connected `);})
