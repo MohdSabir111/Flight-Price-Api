64c0b18cd6fe87ad7c1ebf0a76e7b15efd8d7afa
MOHD SABIR <sabiransari01981@gmail.com>
2023-06-06 01:54:48 +0530

    .

--- models/conn.js
+++ models/conn.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/Flight-Price',{ connectTimeoutMS: 30000,})
+mongoose.connect('mongodb://localhost:27017/Flight-Price',
+{ connectTimeoutMS: 30000, bufferCommands: false,})
 .then(()=>{console.log('database connection is sucessfull');})
 .catch((error)=>{console.log(`${error} not connected `);})
