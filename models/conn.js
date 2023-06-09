99ca1630f68c9773486145f66260e132ef0e6b35
MOHD SABIR <sabiransari01981@gmail.com>
2023-06-10 01:12:53 +0530

    flight api

--- models/conn.js
+++ models/conn.js
@@ -3,8 +3,6 @@ const db ='mongodb+srv://MohdSabir111:coder%40111@flight.8zaqexj.mongodb.net/?re
 mongoose.connect(db,{
     useNewUrlParser : true,
      useUnifiedTopology : true,
-
-
     })
 .then(()=>{console.log('database connection is sucessfull');})
 .catch((error)=>{console.log(`${error} not connected `);})
